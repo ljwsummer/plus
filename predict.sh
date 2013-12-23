ccbfe496db152fcf96d2389435994935bb9de8d8
ljwsummer <ljwsummer@gmail.com>
2013-12-23 11:01:15 +0800

    some code comments

--- predict.sh
+++ predict.sh
@@ -1,3 +1,4 @@
 
+# an example of using a svm model to predict
 python predict.py -f data/demo_data.txt -i output/svm_model -m svm -r output/svm_preds
 
--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 
+# an example of training a svm model
 python train.py -c conf/ml.cfg -m svm
 
