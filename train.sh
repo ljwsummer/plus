3d5bfeb852fa6007a7023b46e270033159c0ac85
ljwsummer <ljwsummer@gmail.com>
2014-03-11 03:19:41 -0700

    add an example of training linear_regression model

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
 
 # an example of training a svm model
-python train.py -c conf/ml.cfg -m svm
+#python train.py -c conf/ml.cfg -m svm
+
+# an example of training a linear_regression model
+python train.py -c conf/ml.cfg -m linear_regression
 
