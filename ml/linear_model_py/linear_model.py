0f4ebcdda792b61acbd2f2a3ea1d6836620f6c70
ljwsummer <ljwsummer@gmail.com>
2014-03-11 03:48:57 -0700

    follow google python style guide

--- ml/linear_model_py/linear_model.py
+++ ml/linear_model_py/linear_model.py
@@ -9,58 +9,58 @@ import numpy as np
 import reader
 
 class Error(Exception):
-  pass
+    pass
 
 class LinearModel(object):
-  """base class of linear model"""
+    """base class of linear model"""
 
-  def __init__(self):
-    if self.__class__ is LinearModel:
-      raise Error("this is a abstract class")
-    self.costs = []
+    def __init__(self):
+        if self.__class__ is LinearModel:
+            raise Error("this is a abstract class")
+        self.costs = []
 
-  def _load_data(self, data_file):
-    if data_file != '':
-      self.data = reader.Data(data_file)
-      self.m = self.data.X.shape[0]
-      self.n = self.data.X.shape[1]
-      self.theta = np.mat(np.zeros((self.n, 1)))
+    def _load_data(self, data_file):
+        if data_file != '':
+            self.data = reader.Data(data_file)
+            self.m = self.data.X.shape[0]
+            self.n = self.data.X.shape[1]
+            self.theta = np.mat(np.zeros((self.n, 1)))
 
-  def _compute_cost(self):
-    pass
+    def _compute_cost(self):
+        pass
 
-  def _compute_gradient(self):
-    pass
+    def _compute_gradient(self):
+        pass
 
-  def _gradient_descent(self):
-    grad = self._compute_gradient(self.data.X, self.data.Y, self.theta)
-    self.theta -= self.alpha * grad / self.m
+    def _gradient_descent(self):
+        grad = self._compute_gradient(self.data.X, self.data.Y, self.theta)
+        self.theta -= self.alpha * grad / self.m
 
-  def train(self):
-    self.theta = np.mat(np.zeros((self.n, 1)))
-    for x in xrange(self.iters):
-      self._gradient_descent()
-      cost = self._compute_cost(self.data.X, self.data.Y, self.theta)
-      self.costs.append(cost)
+    def train(self):
+        self.theta = np.mat(np.zeros((self.n, 1)))
+        for x in xrange(self.iters):
+            self._gradient_descent()
+            cost = self._compute_cost(self.data.X, self.data.Y, self.theta)
+            self.costs.append(cost)
 
-  def predict(self, X):
-    delta = X.shape[1] - self.theta.shape[0]
-    if delta < 0:
-      theta = self.theta[:X.shape[1]]
-    elif delta > 0:
-      theta = np.vstack([self.theta, np.mat(np.zeros((delta, 1)))])
-    else:
-      theta = self.theta
-    return X * theta
+    def predict(self, X):
+        delta = X.shape[1] - self.theta.shape[0]
+        if delta < 0:
+            theta = self.theta[:X.shape[1]]
+        elif delta > 0:
+            theta = np.vstack([self.theta, np.mat(np.zeros((delta, 1)))])
+        else:
+            theta = self.theta
+        return X * theta
 
-  def save_model(self, model_out):
-    fp = open(model_out, 'w')
-    t = [str(x) for x in self.theta.flat]
-    print >> fp, ';'.join(t)
-    fp.close()
+    def save_model(self, model_out):
+        fp = open(model_out, 'w')
+        t = [str(x) for x in self.theta.flat]
+        print >> fp, ';'.join(t)
+        fp.close()
 
-  def load_model(self, model_in):
-    fp = open(model_in)
-    self.theta = np.matrix(fp.readline().strip())
-    fp.close()
+    def load_model(self, model_in):
+        fp = open(model_in)
+        self.theta = np.matrix(fp.readline().strip())
+        fp.close()
 
--- ml/linear_model_py/linear_regression.py
+++ ml/linear_model_py/linear_regression.py
@@ -12,84 +12,84 @@ import numpy as np
 from linear_model import LinearModel
 
 class LinearReg(LinearModel):
-  """linear regression"""
+    """linear regression"""
 
-  def __init__(self, options):
-    LinearModel.__init__(self)
-    self.alpha = options.alpha
-    self.iters = options.num_iters
-    self.lambd = options.lambd
-    self.intercept = options.has_intercept
-    self.model_in = options.model_in
-    self.model_out = options.model_out
-    self.training_file = options.training_file
-    self.test_file = options.test_file
-    self.results_file = options.results_file
-    if self.test_file != '':
-      self._load_data(self.test_file)
-    else:
-      self._load_data(self.training_file)
+    def __init__(self, options):
+        LinearModel.__init__(self)
+        self.alpha = options.alpha
+        self.iters = options.num_iters
+        self.lambd = options.lambd
+        self.intercept = options.has_intercept
+        self.model_in = options.model_in
+        self.model_out = options.model_out
+        self.training_file = options.training_file
+        self.test_file = options.test_file
+        self.results_file = options.results_file
+        if self.test_file != '':
+            self._load_data(self.test_file)
+        else:
+            self._load_data(self.training_file)
 
-  def _compute_cost(self, X, Y, theta):
-    m = X.shape[0]
-    res = X * theta - Y
-    idx = 1 if self.intercept else 0
-    j = sum(np.power(res, 2)) / m + self.lambd * sum(np.power(theta[idx:], 2)) / (2 * m)
-    return j[0,0]
+    def _compute_cost(self, X, Y, theta):
+        m = X.shape[0]
+        res = X * theta - Y
+        idx = 1 if self.intercept else 0
+        j = sum(np.power(res, 2)) / m + self.lambd * sum(np.power(theta[idx:], 2)) / (2 * m)
+        return j[0,0]
 
-  def _compute_gradient(self, X, Y, theta):
-    if self.intercept:
-      ret = X.T * (X * theta - Y) + np.vstack([np.matrix('0'), self.lambd * theta[1:]])
-    else:
-      ret = X.T * (X * theta - Y) + self.lambd * theta
-    return ret
+    def _compute_gradient(self, X, Y, theta):
+        if self.intercept:
+            ret = X.T * (X * theta - Y) + np.vstack([np.matrix('0'), self.lambd * theta[1:]])
+        else:
+            ret = X.T * (X * theta - Y) + self.lambd * theta
+        return ret
 
-  def predict(self, X, results_file):
-    pred = LinearModel.predict(self, X)
-    fp = open(results_file, 'w')
-    for v in pred.flat:
-      print >> fp, v
-    fp.close()
+    def predict(self, X, results_file):
+        pred = LinearModel.predict(self, X)
+        fp = open(results_file, 'w')
+        for v in pred.flat:
+            print >> fp, v
+        fp.close()
 
 
 def main():
 
-  if len(sys.argv) <= 1:
-    print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
-    sys.exit(-1)
+    if len(sys.argv) <= 1:
+        print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
+        sys.exit(-1)
 
-  parser = optparse.OptionParser()
-  parser.add_option('-a', '--alpha', dest = 'alpha', type = 'float',
-      default = 0.01, help = 'step size of gradient descent')
-  parser.add_option('-n', '--num_iters', dest = 'num_iters', type = 'int',
-      default = 10000, help = 'number of iterations')
-  parser.add_option('-l', '--lambda', dest = 'lambd', type = 'float',
-      default = 0.001, help = 'regularization factor')
-  parser.add_option('-t', '--has_intercept', dest = 'has_intercept', type = 'int',
-      default = 0, help = '1 / 0 means has intercept or not')
-  parser.add_option('-i', '--model_in', dest = 'model_in', type = 'str',
-      default = '', help = 'read in a model from this file')
-  parser.add_option('-o', '--model_out', dest = 'model_out', type = 'str',
-      default = '', help = 'write the model to this file')
-  parser.add_option('-f', '--training_file', dest = 'training_file', type = 'str',
-      default = '', help = 'file to be used for training')
-  parser.add_option('-e', '--test_file', dest = 'test_file', type = 'str',
-      default = '', help = 'file to be used for testing')
-  parser.add_option('-r', '--results_file', dest = 'results_file', type = 'str',
-      default = '', help = 'file to be used for writing predictions')
+    parser = optparse.OptionParser()
+    parser.add_option('-a', '--alpha', dest = 'alpha', type = 'float',
+            default = 0.01, help = 'step size of gradient descent')
+    parser.add_option('-n', '--num_iters', dest = 'num_iters', type = 'int',
+            default = 10000, help = 'number of iterations')
+    parser.add_option('-l', '--lambda', dest = 'lambd', type = 'float',
+            default = 0.001, help = 'regularization factor')
+    parser.add_option('-t', '--has_intercept', dest = 'has_intercept', type = 'int',
+            default = 0, help = '1 / 0 means has intercept or not')
+    parser.add_option('-i', '--model_in', dest = 'model_in', type = 'str',
+            default = '', help = 'read in a model from this file')
+    parser.add_option('-o', '--model_out', dest = 'model_out', type = 'str',
+            default = '', help = 'write the model to this file')
+    parser.add_option('-f', '--training_file', dest = 'training_file', type = 'str',
+            default = '', help = 'file to be used for training')
+    parser.add_option('-e', '--test_file', dest = 'test_file', type = 'str',
+            default = '', help = 'file to be used for testing')
+    parser.add_option('-r', '--results_file', dest = 'results_file', type = 'str',
+            default = '', help = 'file to be used for writing predictions')
 
-  (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-  linear_reg = LinearReg(options)
+    linear_reg = LinearReg(options)
 
-  if (options.test_file != ''):
-    linear_reg.load_model(options.model_in)
-    linear_reg.predict(linear_reg.data.X, options.results_file)
-  elif (options.training_file != ''):
-    linear_reg.train()
-    linear_reg.save_model(options.model_out)
-  else:
-    print >> sys.stderr, 'both training_file and test_file are null.'
+    if (options.test_file != ''):
+        linear_reg.load_model(options.model_in)
+        linear_reg.predict(linear_reg.data.X, options.results_file)
+    elif (options.training_file != ''):
+        linear_reg.train()
+        linear_reg.save_model(options.model_out)
+    else:
+        print >> sys.stderr, 'both training_file and test_file are null.'
 
 
 if __name__ == '__main__':
