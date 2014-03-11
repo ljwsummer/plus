a9e29cc3da9b61090565050ca79520d6a350c4d0
ljwsummer <ljwsummer@gmail.com>
2014-03-11 03:20:07 -0700

    add an example of predicting linear_regression model

--- predict.sh
+++ predict.sh
@@ -1,4 +1,7 @@
 
 # an example of using a svm model to predict
-python predict.py -f data/demo_data.txt -i output/svm_model -m svm -r output/svm_preds
+#python predict.py -f data/demo_data.txt -i output/svm_model -m svm -r output/svm_preds
+
+# an example of using a linear_regression model to predict
+python predict.py -f data/demo_data.txt -i output/linear_reg_model -m linear_regression -r output/linear_reg_preds
 
