0d3152b9bbce5b94ce6f15731b778405d073197d
ljwsummer <ljwsummer@gmail.com>
2014-03-11 03:34:14 -0700

    follow google python style guide

--- predict.py
+++ predict.py
@@ -14,40 +14,40 @@ binmap = {
 
 
 def predict(options):
-  cmds = []
-  cmds.append(binmap[options.model])
-  cmds.append('--test_file')
-  cmds.append(options.test_file)
-  cmds.append('--model_in')
-  cmds.append(options.model_in)
-  cmds.append('--results_file')
-  cmds.append(options.results_file)
+    cmds = []
+    cmds.append(binmap[options.model])
+    cmds.append('--test_file')
+    cmds.append(options.test_file)
+    cmds.append('--model_in')
+    cmds.append(options.model_in)
+    cmds.append('--results_file')
+    cmds.append(options.results_file)
 
-  print ' '.join(cmds)
-  print subprocess.check_output(cmds)
+    print ' '.join(cmds)
+    print subprocess.check_output(cmds)
 
 
 def main():
-  if len(sys.argv) <= 1:
-    print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
-    sys.exit(-1)
+    if len(sys.argv) <= 1:
+        print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
+        sys.exit(-1)
 
-  parser = optparse.OptionParser(usage = 'usage: %prog -h')
-  parser.add_option('-f', '--test_file', dest = 'test_file',
-      default = '', help = 'file to be used for testing')
-  parser.add_option('-i', '--model_in', dest = 'model_in',
-      default = '', help = 'read in a model from this file')
-  parser.add_option('-m', '--model', dest = 'model',
-      default = '', help = 'choose training algorithm linear_regression/logistic_regression/svm')
-  parser.add_option('-r', '--results_file', dest = 'results_file',
-      default = '', help = 'file to be used for writing predictions')
+    parser = optparse.OptionParser(usage = 'usage: %prog -h')
+    parser.add_option('-f', '--test_file', dest = 'test_file',
+            default = '', help = 'file to be used for testing')
+    parser.add_option('-i', '--model_in', dest = 'model_in',
+            default = '', help = 'read in a model from this file')
+    parser.add_option('-m', '--model', dest = 'model',
+            default = '', help = 'choose training algorithm linear_regression/logistic_regression/svm')
+    parser.add_option('-r', '--results_file', dest = 'results_file',
+            default = '', help = 'file to be used for writing predictions')
 
-  (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-  predict(options)
+    predict(options)
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
 
--- train.py
+++ train.py
@@ -3,50 +3,49 @@
 
 import sys
 import subprocess
-import os
 import ConfigParser
 import optparse
 
 
 def train(model, config_file, optparser):
-  config = ConfigParser.ConfigParser()
-  config.optionxform = str
-  config.read(config_file)
+    config = ConfigParser.ConfigParser()
+    config.optionxform = str
+    config.read(config_file)
 
-  options = config.items(model)
+    options = config.items(model)
 
-  cmds = []
-  for name, value in options:
-    if optparser.has_option('--' + name):
-      value = optparser.get_option('--' + name)
-    if name == 'bin':
-      cmds.insert(0, value)
-    else:
-      cmds.append('--' + name)
-      cmds.append(value)
+    cmds = []
+    for name, value in options:
+        if optparser.has_option('--' + name):
+            value = optparser.get_option('--' + name)
+        if name == 'bin':
+            cmds.insert(0, value)
+        else:
+            cmds.append('--' + name)
+            cmds.append(value)
 
-  print ' '.join(cmds)
-  print subprocess.check_output(cmds)
+    print ' '.join(cmds)
+    print subprocess.check_output(cmds)
 
 
 def main():
-  if len(sys.argv) <= 1:
-    print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
-    sys.exit(-1)
+    if len(sys.argv) <= 1:
+        print >> sys.stderr, 'usage: ' + sys.argv[0] + ' -h'
+        sys.exit(-1)
 
-  parser = optparse.OptionParser(usage = "usage: %prog -h")
-  parser.add_option('-c', '--config', dest = 'config_file',
-      default = 'conf/ml.cfg',
-      help = 'config file')
-  parser.add_option('-m', '--model', dest = 'model',
-      default = 'svm',
-      help = 'choose training algorithm linear_regression/logistic_regression/svm')
+    parser = optparse.OptionParser(usage = "usage: %prog -h")
+    parser.add_option('-c', '--config', dest = 'config_file',
+            default = 'conf/ml.cfg',
+            help = 'config file')
+    parser.add_option('-m', '--model', dest = 'model',
+            default = 'svm',
+            help = 'choose training algorithm linear_regression/logistic_regression/svm')
 
-  (options, args) = parser.parse_args()
+    (options, args) = parser.parse_args()
 
-  train(options.model, options.config_file, parser)
+    train(options.model, options.config_file, parser)
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
